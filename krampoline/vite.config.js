348a17cc224619309fa69d3adbc0605592337113
sukjune.choi <sukjune.choi@gmail.com>
2023-09-06 17:30:57 +0900

    efsrwer

--- krampoline/vite.config.js
+++ krampoline/vite.config.js
@@ -3,8 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const staticServerUri = process.env.REACT_APP_PATH || "";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: `/${staticServerUri}/`,
   plugins: [
     vue(),
   ],
