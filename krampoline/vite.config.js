6c383c621e0de579c5435a2e3d9abcb2471716de
sukjune.choi <sukjune.choi@gmail.com>
2023-09-14 14:19:07 +0900

    yrdy

--- krampoline/vite.config.js
+++ krampoline/vite.config.js
@@ -1,10 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const env = loadEnv( process.cwd(), '')
+
+const staticServerUri = env.VITE_A || "";    
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${staticServerUri}/`,
   plugins: [
     vue(),
   ],
