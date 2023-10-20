0b222f7add5728733d48b363c812e33e8dace314
choisukjune <sukjune.choi@gmail.com>
2023-10-20 13:57:14 +0900

    Update vite.config.js

--- krampoline/vite.config.js
+++ krampoline/vite.config.js
@@ -1,16 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-const env = loadEnv( process.cwd(), '')
-
-const staticServerUri = env.VITE_A || "";    
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${staticServerUri}/`,
   plugins: [
     vue(),
   ],
